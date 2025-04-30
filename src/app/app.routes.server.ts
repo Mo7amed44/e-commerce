edd227edd7e5d7948326ff6f236b6ecdbc2d21aa
Mo7amed44 <em8356678@gmail.com>
2025-04-30 15:37:11 +0300

    ecommerce

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'subcategories/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
